034824b4f70323b25bd031b6321ebc19164a8d21
Trey Dockendorf <treydock@tamu.edu>
2016-01-08 12:21:33 -0600

    Add job_submit/gres_env plugin

---
+++ src/plugins/job_submit/gres_env/job_submit_gres_env.c
@@ -0,0 +1,34 @@
+/*****************************************************************************\
+ *  job_submit_defaults.c - Set defaults in job submit request specifications.
+ *****************************************************************************/
+
+#include <slurm/slurm.h>
+#include <slurm/slurm_errno.h>
+
+#include "src/slurmctld/slurmctld.h"
+
+#include <stdio.h>
+
+const char plugin_name[]="Save script jobsubmit plugin";
+const char plugin_type[]="job_submit/gres_env";
+const uint32_t plugin_version   = SLURM_VERSION_NUMBER;
+
+extern int job_submit(struct job_descriptor *job_desc, uint32_t submit_uid, char **err_msg) {
+
+	if( job_desc->tres_per_node != NULL){
+		if( env_array_append (&(job_desc->spank_job_env), "GRES", job_desc->tres_per_node) ){
+			job_desc->spank_job_env_size++;
+		}
+		if (env_array_append (&(job_desc->environment), "SLURM_JOB_GRES", job_desc->tres_per_node)) {
+			job_desc->env_size++;
+		}
+	}
+
+	return SLURM_SUCCESS;
+}
+
+int job_modify(struct job_descriptor *job_desc, struct job_record *job_ptr,
+	       uint32_t submit_uid)
+{
+	return SLURM_SUCCESS;
+}
