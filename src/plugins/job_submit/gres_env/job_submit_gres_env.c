e6181889bc76b8168b96782e09be9aa95df7fd50
Trey Dockendorf <tdockendorf@osc.edu>
2020-07-15 09:55:58 -0400

    Add debugging

--- src/plugins/job_submit/gres_env/job_submit_gres_env.c
+++ src/plugins/job_submit/gres_env/job_submit_gres_env.c
@@ -16,10 +16,13 @@ const uint32_t plugin_version   = SLURM_VERSION_NUMBER;
 extern int job_submit(struct job_descriptor *job_desc, uint32_t submit_uid, char **err_msg) {
 
 	if( job_desc->tres_per_node != NULL){
+		debug("job_submit/gres_env: tres_per_node detected");
 		if( env_array_append (&(job_desc->spank_job_env), "GRES", job_desc->tres_per_node) ){
+			debug("job_submit/gres_env: set SPANK_GRES to %s", job_desc->tres_per_node);
 			job_desc->spank_job_env_size++;
 		}
 		if (env_array_append (&(job_desc->environment), "SLURM_JOB_GRES", job_desc->tres_per_node)) {
+			debug("job_submit/gres_env: set SLURM_JOB_GRES to %s", job_desc->tres_per_node);
 			job_desc->env_size++;
 		}
 	}
